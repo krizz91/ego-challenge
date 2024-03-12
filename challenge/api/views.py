bf1c2000065f0eadf85622cb441062bbbf374793
Christian Keegan <keeganchris91@gmail.com>
2024-03-12 20:58:28 -0300

    Added filter

--- challenge/api/views.py
+++ challenge/api/views.py
@@ -1,8 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.permissions import AllowAny
 
 from challenge import serializers
-from challenge.models import Car
+from challenge.models import Car, CarType
 
 class CarListView(ListAPIView):
     """
@@ -11,6 +12,8 @@ class CarListView(ListAPIView):
     permission_classes = (AllowAny, )
     queryset = Car.objects.all()
     serializer_class = serializers.CarSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['car_type']
 
 
 class GetCarView(RetrieveAPIView):
@@ -18,3 +21,8 @@ class GetCarView(RetrieveAPIView):
     queryset = Car.objects.all()
     serializer_class = serializers.CarSerializer
     lookup_field = 'id'
+
+class CarTypeListView(ListAPIView):
+    permission_classes = (AllowAny, )
+    queryset = CarType.objects.all()
+    serializer_class = serializers.CarTypeSerializer
--- challenge/serializers.py
+++ challenge/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from challenge.models import Car
+from challenge.models import Car, CarType
 
 class CarSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Car
+        fields = '__all__'
+
+class CarTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CarType
         fields = '__all__'
